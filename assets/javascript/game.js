6ebdc7c108d03b6636e6649484a31241ded6c744
Keegan Kelly <keegankelly1@gmail.com>
2016-08-17 08:31:19 -0500

    script updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,49 @@
-var gameWords = ["chicken", "pig", "cow"]
+var gameWords = ["pig", "chicken", "cow"];
 var word = gameWords[Math.floor(Math.random()*gameWords.length)];
-var guesses = 10
+var blanks = [];
+var guesses = 10;
 
+for (var i = 0; i < word.length; i++){
+	blanks.push("_")
+}
+console.log(blanks);
 console.log(gameWords);
 console.log(word);
 console.log(guesses);
 
-if (guesses === 0){
-	alert("You lose!");
-	location.reload();	
+function checkGuesses(){
+	if (guesses < 0){
+		alert("You lose!");
+		location.reload();	
+	}
 }
 
-document.onkeyup = function(){
-	
+function checkWords(){
+	for(var i = 0; i < word.length; i++){
+		if (key === word.charAt(i)){
+			blanks[i] = key;
+			var check = true;
+		}
+
+		else{
+			var check = false;
+		}
+	}
+}
+
+document.onkeyup=function(){
+	var x = event.key;
+	var key = x.toLowerCase();
+	console.log(key);
+	checkGuesses();
+	console.log(guesses);
+	checkWords();
+
+	if (check === false){
+		guesses--;
+	}
+
+	else{
+			console.log("Good guess!");
+	}
 }
