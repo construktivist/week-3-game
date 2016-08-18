45ed00f7d516526235bd93b0b84e6d031c890418
Keegan Kelly <kkelly@ncsoft.corp>
2016-08-18 17:05:01 -0500

    script udpate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,52 @@
 var gameWords = ["pig", "chicken", "cow"];
 var word = gameWords[Math.floor(Math.random()*gameWords.length)];
 var blanks = [];
+var guessedLetters = [];
 var guesses = 10;
 
 for (var i = 0; i < word.length; i++){
-	blanks.push("_")
+	blanks.push("_");
 }
-console.log(blanks);
+
 console.log(gameWords);
+console.log(blanks);
 console.log(word);
 console.log(guesses);
 
+document.onkeyup=function(){
+	var x = event.key;
+	var key = x.toLowerCase();
+	var match = false;
+	checkGuesses();
+	console.log(key);
+	guessedLetters.push(key);
+	for(var i = 0; i < word.length; i++){
+		if (key === word.charAt(i)){
+			console.log("match");
+			blanks[i] = key;
+			var match = true;
+			console.log(blanks);
+		}
+		else{
+			console.log("key does not match " + word.charAt(i));
+		}
+	}
+	console.log(match);
+	
+	if (match === true){
+		console.log("Good guess!")
+		console.log(guessedLetters);
+	}	
+	else{
+		guesses--
+		console.log(guesses)
+		console.log(guessedLetters);
+	}
+}
+
 function checkGuesses(){
-	if (guesses < 0){
-		alert("You lose!");
+	if (guesses < 1){
+		alert("You lose. Try again!");
 		location.reload();	
 	}
 }
-
-document.onkeyup=function(){
-	checkGuesses();
-	var x = event.key;
-	var key = x.toLowerCase();
-	console.log(key);
-	
-	for(var i = 0; i < word.length; i++){
-		if (key === word.charAt(i)){
-			blanks[i] = key;
-		}
-
-		else{
-			guesses--;
-			console.log(guesses);
-		}
-	}
-}
-
-
-
