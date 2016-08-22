ee73895311af868a977b91f1f435e1893a5e68b3
Keegan Kelly <kkelly@ncsoft.corp>
2016-08-22 17:20:15 -0500

    script update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,92 +1,85 @@
+var wins = 0;
 initialize();
 
-var wins = 0;
-var guessedLetters = [];
-var guesses = 15;
-var gameWords = ["pig", "chicken", "cow","dog", "cat", "spider"];
-var word = gameWords[Math.floor(Math.random()*gameWords.length)]; // Selects random word from array and makes is a variable.
-var blanks = [];
-var wordArray = [];
-
 function initialize(){
-	var gameWords = ["pig", "chicken", "cow","dog", "cat", "spider"];
-	var word = gameWords[Math.floor(Math.random()*gameWords.length)]; // Selects random word from array and makes is a variable.
+	var gameWords = ["pig", "chicken", "cow"];
+	var word = gameWords[Math.floor(Math.random()*gameWords.length)];
 	var wordArray = [];
 	var blanks = []; // Holds blanks.
 	var guessedLetters = [];
-	var guesses = 15;
-	for (var i = 0; i < word.length; i++){
-		blanks.push("_");
-	}
+	var guesses = 15; //# of guesses.
 
-	for (var i = 0; i < word.length; i++){
-		wordArray.push(word.charAt(i));
-	}
-}
+		for (var i = 0; i < word.length; i++){
+			wordArray.push(word.charAt(i));
+		}
+		
+		 for (var i = 0; i < word.length; i++){
+			blanks.push("_");
+		}
+		document.getElementById("current-word").innerHTML = blanks;
 
-// For debugging.
-// console.log(gameWords);
-// console.log(blanks);
-// console.log(word);
-// console.log(wordArray);
-// console.log(guesses);
+	// For debugging.
+	// console.log(gameWords);
+	// console.log(blanks);
+	// console.log(word);
+	// console.log(wordArray);
+	// console.log(guesses);
+	//Function that startes the game on key select
+	document.onkeyup=function(){
+		var x = event.key;
+		var key = x.toLowerCase();
+		if(guessedLetters.indexOf(key) !== -1){
+			return;
+		}
+		guessedLetters.push(key);
+		var match = false;
+		checkGuesses();
+		console.log(key);
 
+		for(var i = 0; i < word.length; i++){
+			if (key === word.charAt(i)){
+			console.log("match");
+			blanks[i] = key;
+			var match = true;
+			console.log(blanks);
+			}
 
-//Function that startes the game on key select
-document.onkeyup=function(){
-	var x = event.key;
-	var key = x.toLowerCase();
-	if(guessedLetters.indexOf(key) !== -1){
-		return;
-	}
-	guessedLetters.push(key);
-	var match = false;
-	checkGuesses();
-	console.log(key);
+			else{
+			console.log("key does not match " + word.charAt(i));
+			}
+		}
 
-	for(var i = 0; i < word.length; i++){
-		if (key === word.charAt(i)){
-		console.log("match");
-		blanks[i] = key;
-		var match = true;
-		console.log(blanks);
+		console.log(match);
+		if (match === true){
+			console.log("Good guess!");
+			console.log(guessedLetters);
 		}
 
 		else{
-		console.log("key does not match " + word.charAt(i));
+			guesses--
+			console.log(guesses);
+			console.log(guessedLetters);
+		}
+
+		if(blanks.join() == wordArray.join()){
+			alert("You won!");
+			wins++;
+			console.log(wins);
+			initialize();
+		}
+		document.getElementById("current-word").innerHTML = blanks;
+		document.getElementById("guess-number").innerHTML = guesses;
+		document.getElementById("guessed-letters").innerHTML = guessedLetters;
+	}
+
+	function checkGuesses(){
+		if (guesses < 1){
+			alert("You lose. Try again!");
+			initialize();	
 		}
 	}
 
-	console.log(match);
-	if (match === true){
-		console.log("Good guess!");
-		console.log(guessedLetters);
-	}
-
-	else{
-		guesses--
-		console.log(guesses);
-		console.log(guessedLetters);
-	}
-
-	if(blanks.join() == wordArray.join()){
-		alert("You won!");
-		wins++;
-		console.log(wins);
-		initialize();
-	}
-	document.getElementById("current-word").innerHTML = blanks;
-	document.getElementById("guess-number").innerHTML = guesses;
-	document.getElementById("guessed-letters").innerHTML = guessedLetters;
 }
-
-function checkGuesses(){
-	if (guesses < 1){
-		alert("You lose. Try again!");
-		initialize();	
-	}
-}
-
 
 
 
